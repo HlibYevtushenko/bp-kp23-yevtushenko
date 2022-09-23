6fa25f1302df1f63c9c9c2836c1d1ce130406cdb
HlibYevtushenko <glab0742@gmail.com>
2022-09-23 21:48:31 +0300

    Summ

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Programming
+﻿using System;
+
+namespace Programming
 {
     class Program
     {
@@ -6,6 +8,14 @@
         {
             Console.WriteLine("Hello world!");
             Console.WriteLine("Hello world!");
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            int b = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            int c = a + b;
+            Console.WriteLine(c);
+
+
 
 
         }
